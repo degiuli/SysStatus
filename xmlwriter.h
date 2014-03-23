34d2adcfb395bdbe38b796ee9db6f5d356474306
Fábio Lourenção De Giuli <degiuli@outlook.com>
2014-03-23 12:48:14 -0300

    Converted to VS2013

--- xmlwriter.h
+++ xmlwriter.h
@@ -1,3 +1,26 @@
+/*--
+The MIT License (MIT)
+
+Copyright (c) 2010-2013 De Giuli Informática Ltda. (http://www.degiuli.com.br)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+--*/
+
 //ASCII: ANSI_X3.4-1968, ANSI_X3.4-1986, ASCII, ISO646-US, US-ASCII, ascii7, iso-ir-6, us, Windows-20127 
 //Double Byte Character Set: GB2312.1980-0, chinese, gb2312-1980, iso-ir-58 
 //HZ: HZ, HZ-GB-2312 
